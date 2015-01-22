1fa40facf7c72bfbccfdcba9d1d65a58f822644a
Gregory Eric Sanderson <gzou2000@gmail.com>
2015-01-22 14:34:51 -0500

    add small utility for debugging midi files

---
+++ utils/mido_read.py
@@ -0,0 +1,10 @@
+import mido
+import sys
+
+filename = sys[1]
+
+mid = mido.MidiFile(filename)
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for message in track:
+        print(message)
